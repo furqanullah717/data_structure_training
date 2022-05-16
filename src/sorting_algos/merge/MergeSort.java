6b90368f6b781af05e090eaabd78166f9b5ff809
Furqan Ullah <furqanullah717@gmail.com>
2022-05-16 14:40:05 +0500

    merge sort updated

--- src/sorting_algos/merge/MergeSort.java
+++ src/sorting_algos/merge/MergeSort.java
@@ -7,33 +7,50 @@ public class MergeSort implements SortingAlgo {
     @Override
     public Integer[] sort(Integer[] integers) {
 
-        mergeSort(integers, 0, integers.length);
+        mergeSort(integers);
         return integers;
     }
 
-    public void mergeSort(Integer[] items, int start, int end) {
-        if (end - start < 2) {
+    public void mergeSort(Integer[] items) {
+        if (items.length < 2) {
             return;
         }
-        int mid = (start + end) / 2;
-        mergeSort(items, start, mid);
-        mergeSort(items, mid, end);
-        merge(items, start, mid, end);
+        //identify then mid
+        int mid = items.length / 2;
+
+        //create 2 temp arrays for left side and right side
+        Integer[] leftSide = new Integer[mid];
+        Integer[] rightSide = new Integer[items.length - mid];
+
+        //fil data inside the temp arrays
+        for (int i = 0; i < mid; i++) {
+            leftSide[i] = items[i];
+        }
+        for (int i = mid; i < items.length; i++) {
+            rightSide[i - mid] = items[i];
+        }
+
+        //divide left side
+        mergeSort(leftSide);
+        //divide right side
+        mergeSort(rightSide);
+        //merge
+        merge(items, leftSide, rightSide);
 
     }
 
-    void merge(Integer[] items, int start, int mid, int end) {
-        if (items[mid - 1] <= items[mid])
-            return;
-
-        int i = start;
-        int j = mid;
+    void merge(Integer[] items, Integer[] left, Integer[] right) {
+        int i = 0;
+        int j = 0;
         int tempIndex = 0;
-        Integer[] temp = new Integer[end - start];
-        while (i < mid && j < end) {
-            temp[tempIndex++] = items[i] <= items[j] ? items[i++] : items[j++];
+        while (i < left.length && j < right.length) {
+            items[tempIndex++] = left[i] <= right[j] ? left[i++] : right[j++];
         }
-        System.arraycopy(items, i, items, start + tempIndex, mid - i);
-        System.arraycopy(temp, 0, items, start, tempIndex);
+        while (i < left.length) {
+            items[tempIndex++] = left[i++];
+        }
+        while (j < right.length) {
+            items[tempIndex++] = right[j++];
+        }
     }
 }
